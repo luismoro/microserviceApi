5fb9efce414e58579a6aaecf141dd350179f6b6b
Luis Moro <lf.moro@gmail.com>
2020-09-01 17:56:13 -0400

    First commit to Api APM

--- src/main/java/br/microserviceApi/Api/ApiService.java
+++ src/main/java/br/microserviceApi/Api/ApiService.java
@@ -25,7 +25,11 @@ public class ApiService {
         logger.debug("Microservice - 8080" + nowTime.toString());
 
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForEntity(getUrl(ports), String.class).getBody();
+        String url = getUrl(ports);
+        if (url != null)
+            return restTemplate.getForEntity(url, String.class).getBody();
+        else
+            return "";
     }
 
     public String getAsyncService(List ports) throws InterruptedException {
@@ -58,7 +62,7 @@ public class ApiService {
     }
 
         private String getUrl(List ports) {
-            String uri = new String();
+            String uri = null;
 
             if (!ports.isEmpty() && ports.get(0)!= null) {
                 String port = ports.get(0).toString();
--- src/main/java/br/microserviceApi/ApiApplication.java
+++ src/main/java/br/microserviceApi/ApiApplication.java
@@ -1,5 +1,6 @@
 package br.microserviceApi;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
     public static void main(String[] args) {
+        ElasticApmAttacher.attach();
+
         SpringApplication.run(ApiApplication.class, args);
     }
 
